8019d222e028cc3f89f4690084f37a93eceb3cbc
zhuxj <xianjunzhu1985@hotmail.com>
2012-12-20 16:23:33 +0800

    基类增加jdbcTemplate

--- src/main/java/com/willow/platform/core/base/dao/BaseDao.java
+++ src/main/java/com/willow/platform/core/base/dao/BaseDao.java
@@ -17,6 +17,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +38,8 @@ public abstract class BaseDao<E> {
      */
     private final static Logger logger = LoggerFactory.getLogger(BaseDao.class);
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Autowired
     private SqlSessionTemplate sqlSessionTemplate;
 
@@ -362,6 +365,18 @@ public abstract class BaseDao<E> {
 
     //mybatis映射类名
     public abstract Class getMapperClass();
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public SqlSessionTemplate getSqlSessionTemplate() {
+        return sqlSessionTemplate;
+    }
 
     public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
         this.sqlSessionTemplate = sqlSessionTemplate;
